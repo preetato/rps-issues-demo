f1bfe40dc95997865a0bd5719ed334135b54f371
preetato <70622080+preetato@users.noreply.github.com>
2026-04-09 22:27:03 -0400

    Refactor rock-paper-scissors game loop

--- rps.py
+++ rps.py
@@ -3,26 +3,37 @@
 #   https://realpython.com/python-rock-paper-scissors/
 
 import random
+while True:
+    user_action = input("Enter throw (rock, paper, scissors) or quit: ").lower()
 
-user_action = input("Enter throw (rock, paper, scissors): ")
-ai_action = random.choice(["rock", "paper", "scissors"])
+    if user_action == "quit":
+        print("Thanks for playing!")
+        break
 
-print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+    if user_action not in ["rock", "paper", "scissors"]:
+        print("Invalid input. Try again.\n")
+        continue
 
-if user_action == ai_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "rock":
-    if ai_action == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "paper":
-    if ai_action == "rock":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "scissors":
-    if ai_action == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+    ai_action = random.choice(["rock", "paper", "scissors"])
+
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+
+    print()
